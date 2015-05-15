4038157a6f0257d459c22f81a4ce3de3122aea99
Jefferson Choi <jeffersonchoi@gmail.com>
2015-05-14 17:05:29 -0700

    working connection of board, but not working playermove

--- js/controller.js
+++ js/controller.js
@@ -1,57 +1,88 @@
 angular
-	.module('firettt')
-	.controller("MainController", mainController)
+    .module('firettt')
+    .controller("MainController", mainController)
 
-	mainController.$inject = ['$firebaseObject'];
+    mainController.$inject = ['$firebaseObject'];
 
+    function mainController($firebaseObject) {
 
-	function mainController($firebaseObject) {
-		var self = this;
-		self.counter = 1;
-		self.p1move=false;
-		self.p2move=false;
-		self.decidePlayerMove=decidePlayerMove;
-		self.what=whatever();
+        var self = this;
+        self.counter = 1;
+        self.decidePlayerMove = decidePlayerMove;
+        self.nameChange = nameChange;
 
-		self.game={gridList: ["","","","","","","","",""]}
+        self.playerOne = "Player 1";
+        self.playerTwo = "Player 2";
+        self.showName = true;
+        self.playerOneScore = 0;
+        self.playerTwoScore = 0;
+        
+        self.whatever = whatever();
 
-		function whatever() {
-			var ref = new Firebase("https://jeffersonttt.firebaseio.com/");
-			var what = $firebaseObject(ref);
-			return what;
-		}
+         //Function to connect with firebase
+        function whatever() {
+            var ref = new Firebase("https://jeffersonttt.firebaseio.com/");
+            var what = $firebaseObject(ref);
+            return what;
+            
+                
+        }
+        self.whatever.$loaded(function() {
+            self.whatever.gridList = [{
+                    id: 1, value: ""
+                } , {
+                    id: 2, value: ""
+                } , {
+                    id: 3, value: ""
+                } , {
+                    id: 4, value: ""
+                } , {
+                    id: 5, value: ""
+                } , {
+                    id: 6, value: ""
+                } , {
+                    id: 7, value: ""
+                } , {
+                    id: 8, value: ""
+                } , {
+                    id: 9, value: "" 
+                }];
+                
+                self.whatever.$save();
+            });
 
-		function decidePlayerMove($index) {
-			if (self.counter % 2 !== 0) {
-				self.p1move = !self.p1move;
-				self.counter++;
-				self.what.value="test odd";
-				self.what.$save;
-				switchBoxValue($index);
+        //Function to show/hide the playerName/playerScore class
+        function nameChange() {
+            self.showName = !self.showName;
+        }
 
-			} else if (self.counter % 2 == 0) {
-				self.p2move = !self.p2move;
-				self.counter++;
-				self.what.value="test even";
-				self.what.$save;
-				switchBoxValue($index);
-			}
-		// self.whatever.$add(
-		// 	{value: "empty"}
-		// 	);
-		// self.whatever.$save();
+        // /*
+        // Function to decide player turns
+        // Odd number to determine Player 1 's move = "o"
+        // Even number to determine Player 2 's move = "x"
+        // */
+        function decidePlayerMove($index) {                    
+            if (
+                self.counter % 2 !== 0  
+                && self.whatever.gridList[$index].value == ""
 
-		}
-
-		function switchBoxValue($index) {
-			if (self.p1move == true) {
-				self.game.gridList[$index] = "o";
-				self.p1move = !self.p1move;
-			} else if (self.p2move == true) {
-				self.game.gridList[$index] = "x";
-				self.p2move = !self.p2move;
-			}
-		}
+                )    {
+                    (console.log="!=0")
+                    self.counter++;
+                    self.whatever.gridList[$index].update({id: $index, value: "o"});
+                    (console.log="!=0   o")
+                    } 
+            else if (
+                self.counter % 2 == 0 
+                && self.whatever.gridList[$index].value == ""
+                    (console.log="==0")
+                ) {
+                    self.counter++;
+                    self.whatever.gridList[$index].update({id: $index, value: "x"});
+                    (console.log="==0 x")
+                }
+            self.whatever.$save;
+        }
 
 
 
@@ -61,4 +92,3 @@ angular
 
 
 }
-
